26ac4999933896e504eb45117b05c9a0f8f19c35
Michal78900 <75911188+Michal78900@users.noreply.github.com>
2021-02-01 19:06:59 +0100

    Add files via upload

---
+++ Scp012/Plugin.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using Exiled.API.Enums;
+using Exiled.API.Features;
+
+using PlayerEvent = Exiled.Events.Handlers.Player;
+using ServerEvent = Exiled.Events.Handlers.Server;
+using MapEvent = Exiled.Events.Handlers.Map;
+
+namespace Scp012
+{
+    public class Scp012 : Plugin<Config>
+    {
+        public static Scp012 Singleton;
+
+        public override PluginPriority Priority => PluginPriority.Medium;
+
+        public override string Author => "Michal78900";
+        public override Version Version => new Version(1, 0, 0);
+
+
+        private Handler handler;
+
+        public override void OnEnabled()
+        {
+            base.OnEnabled();
+
+            Singleton = this;
+
+            handler = new Handler(this);
+
+            ServerEvent.WaitingForPlayers += handler.OnWaitingForPlayers;
+            ServerEvent.RoundStarted += handler.OnRoundStart;
+
+            PlayerEvent.PickingUpItem += handler.OnItemPickup;
+            PlayerEvent.DroppingItem += handler.OnItemDrop;
+
+            MapEvent.AnnouncingScpTermination += handler.OnAnnouncingScpTermination;
+
+        }
+
+        public override void OnDisabled()
+        {
+            base.OnDisabled();
+
+            ServerEvent.WaitingForPlayers -= handler.OnWaitingForPlayers;
+            ServerEvent.RoundStarted -= handler.OnRoundStart;
+
+            PlayerEvent.PickingUpItem -= handler.OnItemPickup;
+            PlayerEvent.DroppingItem -= handler.OnItemDrop;
+
+            MapEvent.AnnouncingScpTermination -= handler.OnAnnouncingScpTermination;
+
+            handler = null;
+        }
+    }
+}
